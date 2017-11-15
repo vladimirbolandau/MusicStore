d9805254be09252ee84543a446e92b53c73b0e57
vladimirbolandau <32962866+vladimirbolandau@users.noreply.github.com>
2017-11-15 16:15:29 +0200

    Add class AutofacConfig

---
+++ src/MusicStore/App_Start/AutofacConfig.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System.Web.Mvc;
+using System;
+using MusicStore.Business;
+
+namespace MusicStore.App_Start
+{
+    public static class AutofacConfig
+    {
+        public static void SetUpContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // OPTIONAL: Register model binders that require DI.
+            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinderProvider();
+
+            // OPTIONAL: Register web abstractions like HttpContextBase.
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // OPTIONAL: Enable property injection in view pages.
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // OPTIONAL: Enable property injection into action filters.
+            builder.RegisterFilterProvider();
+
+            SetupDependencies(builder);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private static void SetupDependencies(ContainerBuilder builder)
+        {
+            builder.RegisterType<NewReleasesService>().As<IAlbumsService>();
+        }
+    }
+}
