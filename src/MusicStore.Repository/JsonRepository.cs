7d1815b1a03056a8d822518cf742ce88e2e36c56
vladimirbolandau <32962866+vladimirbolandau@users.noreply.github.com>
2018-01-15 18:08:13 +0200

    Some master changing

--- src/MusicStore.Repository/JsonRepository.cs
+++ src/MusicStore.Repository/JsonRepository.cs
@@ -12,6 +12,7 @@ namespace MusicStore.Repository
                 .DownloadString("https://rss.itunes.apple.com/api/v1/us/apple-music/new-releases/all/50/explicit.json");
             //For reading JSON file correct in UTF8 coding
             //Another commit
+            //Master
             byte[] data = Encoding.Default.GetBytes(jsonWeb);
             json = Encoding.UTF8.GetString(data);
             return json;
