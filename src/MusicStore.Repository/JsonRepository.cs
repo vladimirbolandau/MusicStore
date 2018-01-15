8c246c68e71e564fb9dd414399ed9483e7386dd4
vladimirbolandau <32962866+vladimirbolandau@users.noreply.github.com>
2018-01-15 12:15:06 +0200

    Checking how to work with branches

--- src/MusicStore.Repository/JsonRepository.cs
+++ src/MusicStore.Repository/JsonRepository.cs
@@ -10,6 +10,7 @@ namespace MusicStore.Repository
             string json = null;
             var jsonWeb = new WebClient()
                 .DownloadString("https://rss.itunes.apple.com/api/v1/us/apple-music/new-releases/all/50/explicit.json");
+            //For reading JSON file correct in UTF8 coding
             byte[] data = Encoding.Default.GetBytes(jsonWeb);
             json = Encoding.UTF8.GetString(data);
             return json;
