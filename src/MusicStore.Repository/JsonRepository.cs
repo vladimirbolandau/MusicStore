42bed207fb363e73d5df407646ec994cf616d4e4
vladimirbolandau <32962866+vladimirbolandau@users.noreply.github.com>
2018-01-16 09:41:43 +0200

    New git test

--- src/MusicStore.Repository/JsonRepository.cs
+++ src/MusicStore.Repository/JsonRepository.cs
@@ -5,14 +5,11 @@ namespace MusicStore.Repository
 {
     public class JsonRepository : IJsonRepository
     {
-        public string LoadDataFromApi() //Load JSON File
+        public string LoadDataFromApi()
         {
             string json = null;
             var jsonWeb = new WebClient()
                 .DownloadString("https://rss.itunes.apple.com/api/v1/us/apple-music/new-releases/all/50/explicit.json");
-            //For reading JSON file correct in UTF8 coding
-            //Another commit
-            //Master
             byte[] data = Encoding.Default.GetBytes(jsonWeb);
             json = Encoding.UTF8.GetString(data);
             return json;
